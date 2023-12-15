2ebf76355ca343786bd0c409decc66e871e5b79c
Sergey <sseerrgg223819@gmail.com>
2023-12-15 22:52:41 +0300

    Async/await practice

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿async Task AsyncCounter(int t, int i)
+{
+    await Task.Run(() =>
+    {
+        Thread.CurrentThread.Name = "Поток " + i;
+        Thread.CurrentThread.IsBackground = false;
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"Итерация {i} в Задаче: {Task.CurrentId}, Имя потока: {Thread.CurrentThread.Name}");
+
+            Task.Delay(t).Wait();
+        }
+    });
+}
+void Start()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        AsyncCounter(new Random().Next(10, 50), i);
+    }
+}
+
+Start();
+
+//Console.ReadLine();
+
